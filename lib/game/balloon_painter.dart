c7b293d60a3a85e3e69fcd1660a3be4d0526c7ce
Don Francis <sinhuej@gmail.com>
2025-12-13 22:21:15 -0500

    Extract BalloonPainter into lib/game module

---
+++ lib/game/balloon_painter.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '../ui/skins.dart';
+import 'balloon.dart';
+
+class BalloonPainter extends CustomPainter {
+  final List<Balloon> balloons;
+  final SkinDef skin;
+  final bool frenzy;
+
+  BalloonPainter({
+    required this.balloons,
+    required this.skin,
+    required this.frenzy,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final bgPaint = Paint()..color = skin.background;
+    canvas.drawRect(Offset.zero & size, bgPaint);
+
+    for (final b in balloons) {
+      final paint = Paint()
+        ..color = (b.isGolden ? skin.goldGlowColor : skin.glowColor)
+        ..style = PaintingStyle.fill;
+
+      canvas.drawCircle(b.position, b.radius, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant BalloonPainter oldDelegate) => true;
+}
