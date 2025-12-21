39c62beb6ea3d9fa17cd50cbe05d43c4c2875af0
Don Francis <sinhuej@gmail.com>
2025-12-20 20:13:07 -0500

    Step 6D: add navigation entry point stub

---
+++ lib/navigation/app_navigator.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class AppNavigator {
+  const AppNavigator._();
+
+  static void goToGame(BuildContext context) {
+    // Navigation will be implemented in a future step.
+    // Intentionally left blank.
+  }
+}
