cc32cfc723f6ea2874f58f319c02ff94ef53f0fd
Don Francis <sinhuej@gmail.com>
2025-12-21 17:45:28 -0500

    Infra: remove flutter.ndkVersion and pin explicit NDK version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 }
 
 android {
+    ndkVersion = "27.0.12077973"
     namespace = "com.example.balloon_burst"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
