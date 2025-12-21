f474f82d54549d057db8512cad2a61336eac22f6
Don Francis <sinhuej@gmail.com>
2025-12-20 20:22:46 -0500

    Step 6E: wire AppNavigator goToGame route

--- lib/navigation/app_navigator.dart
+++ lib/navigation/app_navigator.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/widgets.dart';
+import '../routes.dart';
 
 class AppNavigator {
   const AppNavigator._();
 
   static void goToGame(BuildContext context) {
-    // Navigation will be implemented in a future step.
-    // Intentionally left blank.
+    Navigator.of(context).pushReplacementNamed(AppRoutes.game);
   }
 }
