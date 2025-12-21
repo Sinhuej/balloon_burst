a59bd718ede6d4c6f4f3afc09a5a9f6bb211c640
Don Francis <sinhuej@gmail.com>
2025-12-21 04:29:59 -0500

    Step 13-1: add GameplayWorld.popBalloonAt

--- lib/gameplay/gameplay_world.dart
+++ lib/gameplay/gameplay_world.dart
@@ -20,4 +20,28 @@ class GameplayWorld {
     }
     return count;
   }
+
+  /// Step 13-1: explicitly apply a pop to one balloon by index.
+  /// Returns a new world or this world if no change applies.
+  GameplayWorld popBalloonAt(int index) {
+    if (index < 0 || index >= balloons.length) {
+      return this;
+    }
+
+    final target = balloons[index];
+    if (target.isPopped) {
+      return this;
+    }
+
+    final updated = <Balloon>[];
+    for (var i = 0; i < balloons.length; i++) {
+      if (i == index) {
+        updated.add(target.pop());
+      } else {
+        updated.add(balloons[i]);
+      }
+    }
+
+    return GameplayWorld(balloons: updated);
+  }
 }
