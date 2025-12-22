7794e90948ee760cd2b8d671a8f714cb0fdd243e
Don Francis <sinhuej@gmail.com>
2025-12-22 12:17:08 -0500

    Step 43: add monetization hook command (no ads)

---
+++ lib/game/commands/grant_powerup_command.dart
@@ -0,0 +1,8 @@
+import "../powerups/power_up.dart";
+
+/// GrantPowerUpCommand
+/// Stub hook (no ads).
+class GrantPowerUpCommand {
+  final PowerUp powerUp;
+  const GrantPowerUpCommand(this.powerUp);
+}
