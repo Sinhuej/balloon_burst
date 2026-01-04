d782eed1c696d7367f40975b969ede40b7d70e1e
Don Francis <sinhuej@gmail.com>
2026-01-04 14:04:25 -0500

    audio: rewrite AudioPlayerService using MP3 asset

--- lib/audio/audio_player.dart
+++ lib/audio/audio_player.dart
@@ -4,6 +4,7 @@ class AudioPlayerService {
   static final AudioPlayer _player = AudioPlayer();
 
   static Future<void> playPop() async {
+    await _player.stop();
     await _player.play(
       AssetSource('audio/pop.mp3'),
       volume: 1.0,
