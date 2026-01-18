9adce2f5734a4995a384e084009f5a5bcc10131d
Don Francis <sinhuej@gmail.com>
2026-01-17 19:01:38 -0500

    Fix balloon hit detection center and add miss telemetry

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -109,7 +109,7 @@ class _GameScreenState extends State<GameScreen>
       if (b.isPopped) continue;
 
       final bx = centerX + (b.xOffset * _lastSize.width * 0.5);
-      final by = b.y + balloonRadius;
+      final by = b.y - balloonRadius;
 
       final dx = tapPos.dx - bx;
       final dy = tapPos.dy - by;
